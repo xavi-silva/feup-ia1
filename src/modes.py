5d18cfecbe2c1e642a8d827bf84af89740ee5906
Rui Xavier Costa da Silva <xavier.silva@Ruis-MacBook-Pro.local>
2025-03-13 09:37:07 +0000

    switching birds to stack

--- src/modes.py
+++ src/modes.py
@@ -1,6 +1,7 @@
 import pygame
 from branch import Branch
 from bird import Bird
+from collections import deque
 
 # Load images (should be done only once)
 BIRD_IMAGES = [
@@ -22,10 +23,14 @@ BRANCH_IMAGE = pygame.transform.scale(BRANCH_IMAGE, (500, 500))
 
 def easy_mode():
     birds = [Bird(i, BIRD_IMAGES[i]) for i in range(9)]
-    
-    branch1 = Branch(200, 200, [birds[0], birds[0], birds[1]], BRANCH_IMAGE, side="left")
-    branch2 = Branch(900, 400, [birds[1], birds[1], birds[1]], BRANCH_IMAGE, side="right")
-    branch3 = Branch(200, 600, [birds[0], birds[0]], BRANCH_IMAGE, side="left")
+
+    birds1 = deque([birds[0], birds[0], birds[1]])
+    birds2 = deque([birds[1], birds[1], birds[1]])
+    birds3 = deque([birds[0], birds[0]])
+
+    branch1 = Branch(200, 200, birds1, BRANCH_IMAGE, side="left")
+    branch2 = Branch(900, 400, birds2, BRANCH_IMAGE, side="right")
+    branch3 = Branch(200, 600, birds3, BRANCH_IMAGE, side="left")
 
     return [branch1, branch2, branch3]
 
